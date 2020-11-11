3b9b01d66f91616008442929dd1e42f5c025cbd6
Mike Bell <mike.bell@staydomio.com>
2020-11-11 15:12:57 -0500

    chore: init and setup

---
+++ src/Map.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+// import { MapboxDirections } from '@mapbox/mapbox-gl-directions';
+import { TOKEN } from './token';
+import { NEW_YORK_CITY } from './constants';
+import './map.css';
+
+mapboxgl.accessToken = TOKEN;
+
+export const Map = () => {
+  const mapContainer = useRef(null);
+
+  const setupMap = center => {
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center,
+      zoom: 15
+    });
+
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav);
+
+    // const directions = new MapboxDirections({
+    //   accessToken: mapboxgl.accessToken
+    // });
+
+    // map.addControl(directions, 'top-left');
+  }
+
+  useEffect(() => {
+    const successLocation = position => {
+      const { longitude, latitude } = position.coords;
+      setupMap([longitude, latitude]);
+    };
+
+    const errorLocation = () => setupMap(NEW_YORK_CITY);
+
+    navigator.geolocation.getCurrentPosition(
+      successLocation,
+      errorLocation,
+      { enableHighAccuracy: true }
+    );
+  }, []);
+
+
+  return <div ref={mapContainer} className="mapStyle"></div>;
+}
